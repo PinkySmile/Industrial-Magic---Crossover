90d3952c670401285f4f221371982d9c5c425d01
PinkySmile <Gegel85@laposte.net>
2026-02-04 13:06:17 +0100

    Require boost key for boosted jump

--- java/com/rumaruka/emt/item/events/BootsJumpEvent.java
+++ java/com/rumaruka/emt/item/events/BootsJumpEvent.java
@@ -1,9 +1,13 @@
 package com.rumaruka.emt.item.events;
 
 import com.rumaruka.emt.item.armor.boots.ItemElectricBootsTraveller;
+import com.rumaruka.emt.item.armor.boots.ItemNanoBootsTraveller;
+import com.rumaruka.emt.item.armor.boots.ItemQuantumBootsTraveller;
 import ic2.api.item.ElectricItem;
+import ic2.core.IC2;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,43 +20,25 @@ public class BootsJumpEvent {
     public void onPlayerJump(LivingEvent.LivingJumpEvent event) {
         if (event.getEntityLiving() instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-
-
             ItemStack stack = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
 
-            if (stack.getItem() instanceof ItemElectricBootsTraveller)
-                if(ElectricItem.manager.use(stack,100,player)){
+            if (IC2.keyboard.isBoostKeyDown(player)) {
+                Item item = stack.getItem();
 
-                    player.motionY += 0.003f;
-
+                if (item instanceof ItemQuantumBootsTraveller) {
+                    if(ElectricItem.manager.use(stack, 10000, player)) {
+                        player.motionY += 0.75f;
+                    }
+                } else if (item instanceof ItemNanoBootsTraveller) {
+                    if(ElectricItem.manager.use(stack, 1000, player)) {
+                        player.motionY += 0.5f;
+                    }
+                } else if (item instanceof ItemElectricBootsTraveller) {
+                    if (ElectricItem.manager.use(stack, 100, player)) {
+                        player.motionY += 0.25f;
+                    }
                 }
-
-        }
-        if (event.getEntityLiving() instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-
-            ItemStack stack = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
-
-            if (stack.getItem() instanceof ItemElectricBootsTraveller)
-                if(ElectricItem.manager.use(stack,1000,player)){
-                    player.motionY += 0.3333354f;
-
-                }
-
-        }
-        if (event.getEntityLiving() instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-
-
-            ItemStack stack = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
-
-            if (stack.getItem() instanceof ItemElectricBootsTraveller)
-                if(ElectricItem.manager.use(stack,10000,player)){
-                    player.motionY += 0.5f;
-
-                }
+            }
         }
     }
-
-
 }
